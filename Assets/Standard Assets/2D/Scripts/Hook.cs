5a6df746ef68c099053e8d40ac53e97f0f135084
hotsoupisgood <hairyhenry@gmail.com>
2016-03-21 01:01:38 -0700

    Hook Works well
    Hook fires to ball and is able to detach using a fixed joint, still need
    a physical retract and not a teleport

--- Assets/Standard Assets/2D/Scripts/Hook.cs
+++ Assets/Standard Assets/2D/Scripts/Hook.cs
@@ -5,17 +5,23 @@ public class Hook : MonoBehaviour {
     public GameObject hullGO;
     public Rigidbody2D hull, ball;
     private Rigidbody2D hook;
-    public int speedShot = 50, distanceStopRetract = 3;
+    public int speedShot = 50, distanceStopRetract = 3, hookedMass, normMass;
     private float currentSpeedRetract = 0;
     public float speedConstaint = .5f;
     private bool shootHook = false;//shoots hook toward ball
     private bool hooked = false;//once hits ball
     private Vector2 rbDiff;//reusable diff between objects
     private LineRenderer lineRenderer;
+    private Vector2 hullCenterPos;
+    private FixedJoint2D ballToHookJoint;
     // Use this for initialization
     void Start () {
         hook = GetComponent<Rigidbody2D>();
         lineRenderer = GetComponent<LineRenderer>();
+        ballToHookJoint = gameObject.AddComponent<FixedJoint2D>();
+        ballToHookJoint.connectedBody = hull;
+        ballToHookJoint.autoConfigureConnectedAnchor = false;
+        ballToHookJoint.enableCollision = false;
     }
 	
 	// Update is called once per frame
@@ -28,7 +34,7 @@ public class Hook : MonoBehaviour {
         }
         if (hooked)//retract to hull
         {
-            ball.position = hook.position;
+            //ball.position = hook.position;
             if (Vector2.Distance(hull.position, hook.position) > distanceStopRetract)
             {
                 rbDiff = hull.position - hook.position;//destination - obj
@@ -37,11 +43,11 @@ public class Hook : MonoBehaviour {
                 hook.AddForce(rbDiff * currentSpeedRetract);//keep hook at certian distance
             }
         }
-        rbDiff = hook.position - hull.position;//destination - obj
-        rbDiff.Normalize();
-        lineRenderer.SetPosition(0, hull.position);
+        hullCenterPos.x = hull.position.x + Mathf.Cos(hull.transform.eulerAngles.z * Mathf.Deg2Rad);
+        hullCenterPos.y = hull.position.y + Mathf.Sin(hull.transform.eulerAngles.z * Mathf.Deg2Rad);
+        lineRenderer.SetPosition(0, hullCenterPos);
         lineRenderer.SetPosition(1, hook.position);
-        lineRenderer.SetColors(Color.white, Color.white);
+        //lineRenderer.SetColors(Color.white, Color.white);//notfuckingworking!!!!!!
     }
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -53,6 +59,8 @@ public class Hook : MonoBehaviour {
                 shootHook = false;
                 hooked = true;
                 ball = other.rigidbody;
+                ballToHookJoint.connectedBody = ball;
+                setJointEnabled(true);
             }
         } 
     }
@@ -63,6 +71,24 @@ public class Hook : MonoBehaviour {
     public void ToggleShootHook()
     {
 
-        shootHook = !shootHook;
+        if (hooked)
+        {//unhook if hooked
+            hooked = false;
+            setJointEnabled(true);
+            ballToHookJoint.connectedBody = hull;
+            hook.mass = 0;
+            //setJointEnabled(false);
+        }
+        else
+        {
+            shootHook = !shootHook;
+            ballToHookJoint.enabled = false;
+            hook.mass = .2f;
+        }
+    }
+   private void setJointEnabled(bool isEnabled)
+    {
+        ballToHookJoint.enabled = isEnabled;
+
     }
 }
